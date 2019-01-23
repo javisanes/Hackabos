0173f96aaabe2c825cea5b841a10ee711542f0e2
Javier Sanesteban <javiersanesteban@gmail.com>
2019-01-23 17:58:39 +0100

    Actualización

---
+++ Dia1/app.js
@@ -0,0 +1,13 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+let name = "Javier";
+let admin = name;
+
+alert("El administrador es " + admin);
+
+let ourPlanetName = "Neptuno";
+
+let currentUserName;
